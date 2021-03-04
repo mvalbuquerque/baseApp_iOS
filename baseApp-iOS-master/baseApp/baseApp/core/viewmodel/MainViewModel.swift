f1e23eb4fa4b4f705ad11f44e3763527497ded05
@mvalbuquerque <mvalbuquerque@users.noreply.github.com>
2021-03-04 18:41:51 -0300

    Update MainViewModel.swift

--- baseApp-iOS-master/baseApp/baseApp/core/viewmodel/MainViewModel.swift
+++ baseApp-iOS-master/baseApp/baseApp/core/viewmodel/MainViewModel.swift
@@ -2,8 +2,8 @@
 //  MainViewModel.swift
 //  baseApp
 //
-//  Created by Fagron Technologies on 25/04/18.
-//  Copyright © 2018 Fagron Technologies. All rights reserved.
+//  Created by mvalbuquerque on 25/04/18.
+//  Copyright © 2018 mvalbuquerque. All rights reserved.
 //
 
 import Foundation
