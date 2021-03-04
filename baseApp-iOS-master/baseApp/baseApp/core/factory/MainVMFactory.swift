5e2381d4b8b53e9cfacf7ec8249d6e6e8040bfe4
@mvalbuquerque <mvalbuquerque@users.noreply.github.com>
2021-03-04 18:41:00 -0300

    Update MainVMFactory.swift

--- baseApp-iOS-master/baseApp/baseApp/core/factory/MainVMFactory.swift
+++ baseApp-iOS-master/baseApp/baseApp/core/factory/MainVMFactory.swift
@@ -2,7 +2,7 @@
 //  MainVMFactory.swift
 //  baseApp
 //
-//  Created by Fagron Technologies on 25/04/18.
+//  Created by mvalbuquerque on 25/04/18.
 //  Copyright © 2018 Fagron Technologies. All rights reserved.
 //
 
